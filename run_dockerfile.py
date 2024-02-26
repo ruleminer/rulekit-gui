4d52c21a98442f20cd548e0d6916a09c88a1a0cb
szumx123 <150140698+szumx123@users.noreply.github.com>
2024-02-26 13:57:34 +0100

    Update run_dockerfile.py

--- run_dockerfile.py
+++ run_dockerfile.py
@@ -2,6 +2,6 @@ import os
 
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
-os.system("cd " + script_dir)
+os.system("cd " + script_dir + "/..")
 os.system("docker run -p 8501:8501 streamlit-rulekit-gui")
 os.system("start http://localhost:8501")
